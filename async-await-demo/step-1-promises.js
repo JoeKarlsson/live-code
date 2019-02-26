9ddba340415951006d40c259854cdcfc7c3a82d2
Joe Karlsson <josephcarlson1@gmail.com>
2019-02-26 14:36:55 -0600

    async await demo complete.

--- async-await-demo/step-1-promises.js
+++ async-await-demo/step-1-promises.js
@@ -1,7 +1,7 @@
 const resolveAfter2Seconds = () => {
   console.log("starting slow promise");
   return new Promise(resolve => {
-    setTimeout(function() {
+    setTimeout(() => {
       resolve(20);
       console.log("slow promise is done");
     }, 2000);
@@ -11,12 +11,17 @@ const resolveAfter2Seconds = () => {
 const resolveAfter1Second = () => {
   console.log("starting fast promise");
   return new Promise(resolve => {
-    setTimeout(function() {
+    setTimeout(() => {
       resolve(10);
       console.log("fast promise is done");
     }, 1000);
   });
 };
 
-resolveAfter2Seconds().then(message => console.log(message));
-resolveAfter1Second().then(message => console.log(message));
+resolveAfter2Seconds()
+  .then(message => console.log(message))
+  .catch(err => console.error(err));
+
+resolveAfter1Second()
+  .then(message => console.log(message))
+  .catch(err => console.error(err));
