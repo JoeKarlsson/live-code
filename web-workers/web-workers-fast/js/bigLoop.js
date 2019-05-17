e0e6261d395c90930248c6dd9708fa0e412250b1
Joe Karlsson <josephcarlson1@gmail.com>
2019-05-17 13:34:03 -0500

    cleabn

--- web-workers/web-workers-fast/js/bigLoop.js
+++ web-workers/web-workers-fast/js/bigLoop.js
@@ -1,8 +1,8 @@
 // importScripts('../relative/path/lib.min.js', '../../other/lib.js');
 
-for (var i = 0; i <= 20000001; i += 1) {
+for (var i = 0; i <= 20500001; i += 1) {
    var j = i;
-   if(i%8000 === 0 ) {
+   if(i%5000 === 0 ) {
       postMessage({ type: 'status', data: j });
    }
 }
